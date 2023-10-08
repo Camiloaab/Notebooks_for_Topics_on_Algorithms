54c487d57f58bdcf85af3f6d3237bdd49aeecefe
camiloaab <camilo.arias@impossiblecode.ai>
2023-10-07 20:53:02 -0500

    add constants and .gitignore

---
+++ constants.py
@@ -0,0 +1,25 @@
+
+## Some constants that are used repeatedly
+
+colors  =[
+    'cyan',              # Bright sky-blue
+    'magenta',           # Vivid pinkish-purple
+    'lime',              # Bright green
+    'yellow',            # Bright yellow
+    'hotpink',           # Strong pink
+    'deepskyblue',       # Vivid blue
+    'springgreen',       # Medium to bright green
+    'mediumorchid',      # Bright purple
+    'dodgerblue',        # Neon blue
+    'lightcoral',        # Soft red
+    'mediumspringgreen', # Bright light green
+    'lightsalmon',       # Soft orange
+    'blueviolet',        # Neon violet-blue
+    'aqua',              # Aqua blue
+    'chartreuse',        # Yellow-green
+    'mediumvioletred',   # Rich purple-red
+    'lightseagreen',     # Bright turquoise
+    'darkorange',        # Neon orange
+    'lightpink',         # Soft pink
+    'darkturquoise'      # Deep neon blue
+]
